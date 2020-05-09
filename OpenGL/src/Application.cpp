d5d608f812b3a23fa946c4fecb9a48c3a40df35a
jersha <jershafelix@gmail.com>
2020-05-09 15:29:06 +0530

    Buffer Layout Abstraction

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -8,6 +8,7 @@
 
 #include "IndexBuffer.h"
 #include "VertexBuffer.h"
+#include "VertexArray.h"
 
 struct ShaderProgramSource {
 	std::string VertexSource;
@@ -128,10 +129,11 @@ int main(void)
 		GLCall(glGenVertexArrays(1, &vao));
 		GLCall(glBindVertexArray(vao));
 
+		VertexArray va;
 		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
-
-		GLCall(glEnableVertexAttribArray(0));
-		GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+		VertexBufferLayout layout;
+		layout.Push<float>(2);
+		va.AddBuffer(vb, layout);
 
 		IndexBuffer ib(indices, 6);
 
@@ -158,7 +160,7 @@ int main(void)
 			GLCall(glClear(GL_COLOR_BUFFER_BIT));
 			GLCall(glUseProgram(shader));
 			ib.Bind();
-			GLCall(glBindVertexArray(vao));
+			va.Bind();
 
 			GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
