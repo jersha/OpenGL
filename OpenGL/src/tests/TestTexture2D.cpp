f65df3bbe4a7175dd85ad160ea5772bf0e1238d5
jersha <jershafelix@gmail.com>
2020-05-13 09:20:53 +0530

    Batch Rendering - An Introduction

--- OpenGL/src/tests/TestTexture2D.cpp
+++ OpenGL/src/tests/TestTexture2D.cpp
@@ -13,28 +13,34 @@ namespace test {
 		m_Proj(glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f)),
 		m_View(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f)))
 	{
-		float positions[16] = {
+		float positions[32] = {
 			 -50.0f,  -50.0f, 0.0f, 0.0f,//0
 			  50.0f,  -50.0f, 1.0f, 0.0f,//1
 			  50.0f,   50.0f, 1.0f, 1.0f,//2
-			 -50.0f,   50.0f, 0.0f, 1.0f//3
+			 -50.0f,   50.0f, 0.0f, 1.0f,//3
+			  60.0f,   60.0f, 0.0f, 0.0f,//0
+			  80.0f,   60.0f, 1.0f, 0.0f,//1
+			  80.0f,   00.0f, 1.0f, 1.0f,//2
+			  60.0f,   80.0f, 0.0f, 1.0f//3
 		};
 
-		unsigned int indices[] = {
+		unsigned int indices[12] = {
 			0, 1, 2,
-			2, 3, 0
+			2, 3, 0,
+			4, 5, 6,
+			6, 7, 4
 		};
 
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
 		m_VAO = std::make_unique<VertexArray>();
-		m_VertexBuffer = std::make_unique<VertexBuffer>(positions, 4 * 4 * sizeof(float));
+		m_VertexBuffer = std::make_unique<VertexBuffer>(positions, 8 * 4 * sizeof(float));
 		VertexBufferLayout layout;
 		layout.Push<float>(2);
 		layout.Push<float>(2);
 		m_VAO->AddBuffer(*m_VertexBuffer, layout);
-		m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 6);
+		m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 12);
 		m_Shader = std::make_unique<Shader>("res/shaders/Basic.shader");
 		m_Shader->Bind();
 		m_Texture = std::make_unique<Texture>("res/textures/steve.png");
